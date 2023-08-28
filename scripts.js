6ee792ca1508f8de9b92bbf20c7ce479246d3479
tshidiso-codespace <tshidisomoorosi@gmail.com>
2023-08-28 14:01:34 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -35,18 +35,18 @@ let checkin = document.querySelectorAll('.checkin')
 
 checkin[0].style.color = ''
 status_[0].style.color = STATUS_MAP.overdue.color
-reserve[0] = STATUS_MAP.overdue.canReserve ? 'enabled' : 'disabled'
-checkout[0] = STATUS_MAP.overdue.canCheckout ? 'enabled' : 'disabled'
-checkin[0] = STATUS_MAP.overdue.canCheckIn ? 'enabled' : 'disabled'
+reserve[0].disabled = !STATUS_MAP.overdue.canReserve
+checkout[0].disabled = !STATUS_MAP.overdue.canCheckout
+checkin[0].disabled = !STATUS_MAP.overdue.canCheckIn
 
 checkin[1].style.color = ''
 status_[1].style.color = STATUS_MAP.reserved.color
-reserve[1] = STATUS_MAP.reserved.canReserve ? 'enabled' : 'disabled'
-checkout[1] = STATUS_MAP.reserved.canCheckout ? 'enabled' : 'disabled'
-checkin[1] = STATUS_MAP.reserved.canCheckIn ? 'enabled' : 'disabled'
+reserve[1].disabled = !STATUS_MAP.reserved.canReserve
+checkout[1].disabled = !STATUS_MAP.reserved.canCheckout
+checkin[1].disabled = !STATUS_MAP.reserved.canCheckIn
 
 checkin[2].style.color = ''
 status_[2].style.color = STATUS_MAP.shelf.color
-reserve[2] = STATUS_MAP.shelf.canReserve ? 'enabled' : 'disabled'
-checkout[2] = STATUS_MAP.shelf.canCheckout ? 'enabled' : 'disabled'
-checkin[2] = STATUS_MAP.shelf.canCheckIn ? 'enabled' : 'disabled'
+reserve[2].disabled = !STATUS_MAP.shelf.canReserve
+checkout[2].disabled = !STATUS_MAP.shelf.canCheckout
+checkin[2].disabled = !STATUS_MAP.shelf.canCheckIn
